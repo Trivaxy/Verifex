27f195354e2611ffdcc8523db3daa4668f9d08af
Trivaxy <itsmeomar2003@yahoo.com>
2025-03-13 17:09:54 +0300

    Implement equality comparison and hash code generation for VerifexType

--- Verifex/CodeGen/Types/VerifexType.cs
+++ Verifex/CodeGen/Types/VerifexType.cs
@@ -1,10 +1,39 @@
+using System;
 using System.Reflection;
 
 namespace Verifex.CodeGen.Types;
 
-public abstract class VerifexType
+public abstract class VerifexType : IEquatable<VerifexType>
 {
     public abstract string Name { get; }
     
     public abstract TypeInfo IlType { get; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is VerifexType other && Equals(other);
+    }
+
+    public bool Equals(VerifexType? other)
+    {
+        if (other is null)
+            return false;
+            
+        return Name == other.Name && IlType.Equals(other.IlType);
+    }
+
+    public override int GetHashCode() => HashCode.Combine(Name, IlType);
+
+    public static bool operator ==(VerifexType? left, VerifexType? right)
+    {
+        if (left is null)
+            return right is null;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(VerifexType? left, VerifexType? right)
+    {
+        return !(left == right);
+    }
 }
