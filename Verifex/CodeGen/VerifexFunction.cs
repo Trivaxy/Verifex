bdb136aa1a43f1e7bd4e0207e7787a9e3536af63
Trivaxy <itsmeomar2003@yahoo.com>
2025-03-13 17:07:58 +0300

    Implement equality comparison and hash code generation for VerifexFunction

--- Verifex/CodeGen/VerifexFunction.cs
+++ Verifex/CodeGen/VerifexFunction.cs
@@ -4,8 +4,44 @@ using Verifex.CodeGen.Types;
 namespace Verifex.CodeGen;
 
 public class VerifexFunction(string name, ReadOnlyCollection<ParameterInfo> parameters, VerifexType returnType)
+    : IEquatable<VerifexFunction>
 {
     public readonly string Name = name;
     public readonly ReadOnlyCollection<ParameterInfo> Parameters = parameters;
     public readonly VerifexType ReturnType = returnType;
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as VerifexFunction);
+    }
+
+    public bool Equals(VerifexFunction? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        
+        if (!Name.Equals(other.Name)) return false;
+        if (!ReturnType.Equals(other.ReturnType)) return false;
+        
+        if (Parameters.Count != other.Parameters.Count) return false;
+        
+        for (int i = 0; i < Parameters.Count; i++)
+        {
+            if (!Parameters[i].Equals(other.Parameters[i])) return false;
+        }
+    
+        return true;
+    }
+    
+    public override int GetHashCode()
+    {
+        HashCode hash = new HashCode();
+        hash.Add(Name);
+        hash.Add(ReturnType);
+        
+        foreach (var param in Parameters)
+            hash.Add(param);
+        
+        return hash.ToHashCode();
+    }
 }
